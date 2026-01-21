d6e39b526524298ae05796561459f989f8305a7b
Khushi Bhati <khushibhati2413@gmail.com>
2026-01-21 15:32:30 +0530

    Update AIinterview.jsx

--- src/Components/AIinterview.jsx
+++ src/Components/AIinterview.jsx
@@ -313,8 +313,54 @@ const AIinterview = () => {
               Start Recording Answer
             </button>
 
-            <button className="second-btn">Repeat Question</button>
-            <button className="second-btn">Exit Interview</button>
+            <button className="second-btn">
+              <svg
+                className="repeat-circle"
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  cx="12"
+                  cy="12"
+                  r="9"
+                  stroke="#E5E7EB"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  fill="none"
+                />
+
+                <path
+                  d="M5 3v4h4"
+                  stroke="#E5E7EB"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              <span> Repeat Question</span>
+            </button>
+            <button className="second-btn">
+              <svg
+                className="repeat-circle"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+                <polyline points="16 17 21 12 16 7" />
+                <line x1="21" y1="12" x2="9" y2="12" />
+              </svg>
+              <span>Exit Interview</span>
+            </button>
           </footer>
           <p class="record-hint">
             Click "Start Recording Answer" to record your spoken response
@@ -326,3 +372,4 @@ const AIinterview = () => {
 };
 
 export default AIinterview;
+
